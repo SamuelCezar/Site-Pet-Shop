c68bb2b4497a16d1e5bb936c4e88ab5f612e70d7
SamuelCezar <60257787+SamuelCezar@users.noreply.github.com>
2021-02-06 17:11:18 -0300

    Add files via upload

---
+++ service/cliente-service.js
@@ -0,0 +1,81 @@
+const listaClientes = () => {
+
+    return fetch(`http://localhost:3000/profile`).then(resposta => {
+        return resposta.json();
+    });
+
+    //A PARTE DE CIMA SUBSTITUI TODO O CÓDIGO ABAIXO//
+    /*const promise = new Promise((resolve, reject) => {
+        const http = new XMLHttpRequest();
+
+        http.open('GET', 'http://localhost:3000/profile');
+
+        http.onload = () => {
+            if (http.status >= 400) {
+                reject(JSON.parse(http.response));
+            } else {
+                resolve(JSON.parse(http.response));
+            }
+        };
+        http.send();
+    });
+    return promise;*/
+}
+
+const criaCliente = (nome, email) => {
+    return fetch(`http://localhost:3000/profile`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+        .then(resposta => {
+            return resposta.body;
+        })
+}
+
+const removeCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'DELETE'
+    }
+
+    )}
+
+const detalhaCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`).then(resposta => {
+        return resposta.json();
+    });
+}
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    }).then(resposta => {
+        return resposta.json();
+    })
+}
+
+export const clienteService = {
+    listaClientes,
+    criaCliente,
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente,
+}
+
+
+
+
+
+
